4c11f929074ac0d3dd4c370952a043c4e6dd3ab8
dillon zett <dillonmzett@gmail.com>
2023-11-03 11:36:54 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 16
+let num4 = 4
+let mod = num3 / num4
+
+console.log(max)
+const set = [10, 3, 20, 4, 5, 16]
+let max = Math.max(...set)
