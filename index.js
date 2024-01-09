9b1b52431e756bb2728cfd568fb4424e1ee449fe
dillon zett <dillonmzett@gmail.com>
2024-01-09 10:43:14 -0600

    finished

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let num1 = 31
 let num2 = 2
-let multiply = num1 * num2;
+let multiply = num1 * num2 
 
-let random = Math.floor(Math.random() * 10) + 1;
+let random = 1
+Math.random(random)
 
-let num3 = 16
-let num4 = 4
-let mod = num3 / num4
+let num3 = 10
+let num4 = 44
+let mod = num4 % num3
 
-console.log(max)
-const set = [10, 3, 20, 4, 5, 16]
-let max = Math.max(...set)
+let num5 = 19.5
+let max = Math.ceil(num5)
